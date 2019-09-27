12086db45d3b0b7bb3031d62f4d0a0b0af2eddf5
lixp <846358830@qq.com>
2019-09-27 14:05:35 +0800

    shirodemo2：update

--- src/main/java/com/muniao/shirodemo/controller/UserController.java
+++ src/main/java/com/muniao/shirodemo/controller/UserController.java
@@ -1,61 +1,70 @@
 package com.muniao.shirodemo.controller;
 
-import com.muniao.shirodemo.domain.User;
-import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @Controller
-public class UserController {
+public class UserController
+{
 
     @RequestMapping("one")
-    public String getOne(){
+    public String getOne()
+    {
         return "one";
     }
 
     @RequestMapping("two")
-    public String getTwo(){
+    public String getTwo()
+    {
         return "two";
     }
 
     @RequestMapping("unauthorized")
-    public String unauthorized(){
+    public String unauthorized()
+    {
         return "unauthorized";
     }
 
-    @RequestMapping({"/","/index"})
-    public String index(){
-        return"/index";
+    @RequestMapping({"/", "/index"})
+    public String index()
+    {
+        return "/index";
     }
 
     @RequestMapping("/login")
-    public String login(HttpServletRequest request, Map<String, Object> map) throws Exception{
+    public String login(HttpServletRequest request, Map<String, Object> map) throws Exception
+    {
         System.out.println("HomeController.login()");
         // 登录失败从request中获取shiro处理的异常信息。
         // shiroLoginFailure:就是shiro异常类的全类名.
         String exception = (String) request.getAttribute("shiroLoginFailure");
         System.out.println("exception=" + exception);
         String msg = "";
-        if (exception != null) {
-            if (UnknownAccountException.class.getName().equals(exception)) {
+        if (exception != null)
+        {
+            if (UnknownAccountException.class.getName().equals(exception))
+            {
                 System.out.println("UnknownAccountException -- > 账号不存在：");
                 msg = "UnknownAccountException -- > 账号不存在：";
-            } else if (IncorrectCredentialsException.class.getName().equals(exception)) {
+            }
+            else if (IncorrectCredentialsException.class.getName().equals(exception))
+            {
                 System.out.println("IncorrectCredentialsException -- > 密码不正确：");
                 msg = "IncorrectCredentialsException -- > 密码不正确：";
-            } else if ("kaptchaValidateFailed".equals(exception)) {
+            }
+            else if ("kaptchaValidateFailed".equals(exception))
+            {
                 System.out.println("kaptchaValidateFailed -- > 验证码错误");
                 msg = "kaptchaValidateFailed -- > 验证码错误";
-            } else {
-                msg = "else >> "+exception;
+            }
+            else
+            {
+                msg = "else >> " + exception;
                 System.out.println("else -- >" + exception);
             }
         }
